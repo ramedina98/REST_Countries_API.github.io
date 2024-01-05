04816745edfdb1e7e4236b285069a7ddb992c1f4
ramedina98 <b_stone99@outlook.es>
2024-01-05 12:24:00 -0600

    my second commit

---
+++ rest_countries_api/src/App.tsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <p>Hola a todos</p>
+      </header>
+    </>
+  )
+}
+
+export default App

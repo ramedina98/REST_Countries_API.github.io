0a2b6fda7d61df311179cb6477bc286c11b51486
Ricardo Abraham Medina Martin del Campo <111025121+ramedina98@users.noreply.github.com>
2024-01-10 17:54:07 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { HashRouter, Routes, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './css/index.css'
+import Countries from './components/ListOfCountries.tsx'; 
+import Country from './components/Country.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Countries />}></Route>
+          <Route path="country/:id" element={<Country/>}></Route>
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 )
